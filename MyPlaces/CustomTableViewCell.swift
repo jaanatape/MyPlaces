0c5e7e2659df94621468a115e1c3d65db49fb4f9
jaanatape <jaanatape@gmail.com>
2022-01-06 22:48:45 +0300

    Custom TableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Karim Ibragimov on 06.01.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+}
